ab65830e000d15d386dea7159413cdfea1ce4d6a
xiaodongli1986 <xiaodongli@mail.kias.re.kr>
2020-07-28 09:35:26 +0800

    minor changes

--- gitpush.sh
+++ gitpush.sh
@@ -2,7 +2,7 @@ git pull
 git add *.sh
 #git add *.tar.gz
 #git add */J08.noRSD*.tar.gz
-#git add */J08.RSD*.tar.gz
+git add */*.tar.gz
 git add */data.tar.gz
 git add README.md
 git commit -m 'minor changes'
