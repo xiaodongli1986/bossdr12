21d507dd7590936924f3fd8a27cb29b744044eae
xiaodongli1986 <xiaodongli@mail.kias.re.kr>
2020-07-28 09:33:26 +0800

    minor changes

---
+++ README.md
@@ -0,0 +1,5 @@
+
+BOSS DR12 data & mocks splitted in 6 bins, the samples used in arXiv:1609.05476.
+
+
+
---
+++ gitpush.sh
@@ -0,0 +1,9 @@
+git pull
+git add *.sh
+#git add *.tar.gz
+#git add */J08.noRSD*.tar.gz
+#git add */J08.RSD*.tar.gz
+git add */data.tar.gz
+git add README.md
+git commit -m 'minor changes'
+git push
